3f6657f5490b4ceec47f6de0b38f1c29536f45ad
diewland <diewland@gmail.com>
2022-12-07 21:29:14 +0700

    update script

--- gen_json.py
+++ gen_json.py
@@ -1,49 +1,49 @@
-import json, random
+import json, glob, random
 from pprint import pprint as pp
  
 NAME = "PoPose Gaming DAO"
 DESC = "No utility, No roadmap. Just buy PoPose some stuff 👾"
+IMG = "ipfs://xxxxxxxxxxxxxxx/{}_{}_{}_{}.png" # XXX XXX XXX
 ENGINE = "Jigsaw Engine"
 
+SRC_PATH = "./assets/*.png"
 OUTPUT_DIR = "./json"
 SHUFFLE_TIME = 99
 
-CONFIG = (
-    ( 87, "W", "***" ),
-    ( 65, "A", "***" ),
-    ( 83, "S", "***" ),
-    ( 68, "D", "***" ),
-    ( 3,  "X", "***" ),
-)
+if __name__ == '__main__':
 
+    # load items from asset folder
+    items = [ p.split('/')[-1] for p in glob.glob(SRC_PATH) ]
 
-# build chunk
-chunk = []
-for cfg in CONFIG:
-    (qty, key, img) = cfg
-    for i in range(0, qty):
-        # craft
-        metadata = {
-          "name": "***",
-          "description": DESC,
-          "image": img,
-          "attributes": [
-            { "trait_type": "Key",  "value": key },
-          ],
-          "compiler": ENGINE,
-        }
-        # add to chunk
-        chunk.append(metadata)
+    # build chunk
+    chunk = []
+    for item in items:
+        (keys, head, bg, qty) = item.split('.')[0].split('_')
+        for i in range(0, int(qty)):
+            # craft
+            metadata = {
+              "name": "***",
+              "description": DESC,
+              "image": IMG.format(keys, head, bg, qty),
+              "attributes": [
+                { "trait_type": "Keys",  "value": keys },
+                { "trait_type": "Head",  "value": head },
+                { "trait_type": "Background",  "value": bg },
+              ],
+              "compiler": ENGINE,
+            }
+            # add to chunk
+            chunk.append(metadata)
 
-# shuffle
-for rnd in range(1, SHUFFLE_TIME+1):
-    random.shuffle(chunk)
+    # shuffle
+    for rnd in range(1, SHUFFLE_TIME+1):
+        random.shuffle(chunk)
 
-# update name
-for idx, metadata in enumerate(chunk):
-    metadata["name"] = "{} #{}".format(NAME, idx+1)
+    # update name
+    for idx, metadata in enumerate(chunk):
+        metadata["name"] = "{} #{}".format(NAME, idx+1)
 
-# write file
-for idx, metadata in enumerate(chunk):
-    with open("./{}/{}.json".format(OUTPUT_DIR, idx+1), "w") as f:
-        json.dump(metadata, f, ensure_ascii=False)
+    # write file
+    for idx, metadata in enumerate(chunk):
+        with open("./{}/{}.json".format(OUTPUT_DIR, idx+1), "w") as f:
+            json.dump(metadata, f, ensure_ascii=False)
